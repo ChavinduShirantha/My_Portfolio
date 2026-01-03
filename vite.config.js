2aada3c87329344f20a59ccf2ecc8ddb8450bf78
ChavinduShirantha <chavindu.softedgelab@gmail.com>
2026-01-03 11:59:07 +0530

    Fix vite base URL for Firebase deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: mode === 'production' ? 'https://chavindu-shirantha.web.app/' : './',
+  base: '/',
   define: {
     'import.meta.env.VITE_BASE_URL': JSON.stringify(
       mode === 'production'
